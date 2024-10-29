3bd3034194caac135347dd5965d63055475db170
Taiwo Olapade <semiteprofessor@gmail.com>
2024-10-29 18:06:24 +0100

    Commit

--- ecommerce/services/product_service.py
+++ ecommerce/services/product_service.py
@@ -6,7 +6,7 @@ from ecommerce.utils.response_helper import create_response
 def list_items():
     try:
         query = """
-            SELECT product_name, old_price, new_price, image, rating
+            SELECT product_id, product_name, old_price, new_price, image, rating
             FROM `tabProducts`
             WHERE 1=1
         """
