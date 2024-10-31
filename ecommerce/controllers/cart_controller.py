81a17cd22ef7f69ef8af2a4ba71c8a52bc29c40d
Taiwo Olapade <semiteprofessor@gmail.com>
2024-10-31 22:38:34 +0100

    Commit

--- ecommerce/controllers/cart_controller.py
+++ ecommerce/controllers/cart_controller.py
@@ -7,12 +7,12 @@ def get_cart(user_id):
     return list_cart_items(user_id)
 
 @frappe.whitelist(allow_guest=True)
-def add_item_to_cart(user_id, item_code, product_name, image, seller_name, price, quantity):
-    return add_to_cart(user_id, item_code, product_name, image, seller_name, price, quantity)
+def add_item_to_cart(product_name, image, seller_name, price, quantity):
+    return add_to_cart(product_name, image, seller_name, price, quantity)
 
 @frappe.whitelist(allow_guest=True)
-def update_cart_item(user_id, item_code, product_name, image, seller_name, price, quantity):
-    return update_cart_quantity(user_id, item_code, product_name, image, seller_name, price, quantity)
+def update_cart_item(product_name, image, seller_name, price, quantity):
+    return update_cart_quantity(product_name, image, seller_name, price, quantity)
 
 @frappe.whitelist(allow_guest=True)
 def clear_cart(user_id):
