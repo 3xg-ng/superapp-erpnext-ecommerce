c0586347943182a9321b814a025552f82d92d3ed
Taiwo Olapade <semiteprofessor@gmail.com>
2024-11-01 08:58:32 +0100

    Commit

--- ecommerce/services/cart_service.py
+++ ecommerce/services/cart_service.py
@@ -3,15 +3,15 @@ from ecommerce.constants.http_status import SUCCESS, NOT_FOUND, SERVER_ERROR
 from ecommerce.utils.response_helper import create_response
 
 ### Function to List Cart Items
-def list_cart_items(user_id):
+def list_cart_items():
     try:
         query = """
             SELECT *
             FROM `tabCart`
-            WHERE user_id=%s
+            WHERE 1=1
         """
         
-        cart_items = frappe.db.sql(query, (user_id,), as_dict=True)
+        cart_items = frappe.db.sql(query, as_dict=True)
 
         if not cart_items:
             return create_response(NOT_FOUND, "Cart is empty!")
@@ -19,7 +19,7 @@ def list_cart_items(user_id):
         return create_response(SUCCESS, cart_items)
 
     except Exception as e:
-        error_message = f"Error listing cart items for user {user_id}: {str(e)}"
+        error_message = f"Error listing cart items {str(e)}"
         frappe.log_error(error_message, "Cart Listing Error")
         return create_response(SERVER_ERROR, f"An unexpected error occurred while fetching cart items.")
 
