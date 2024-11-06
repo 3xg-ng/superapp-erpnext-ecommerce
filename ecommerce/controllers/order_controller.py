b9e7a8a6e7fe2488047a9939bbf51516436ad967
Taiwo Olapade <semiteprofessor@gmail.com>
2024-11-06 17:27:41 +0100

    Commit

--- ecommerce/controllers/order_controller.py
+++ ecommerce/controllers/order_controller.py
@@ -5,21 +5,21 @@ from ecommerce.constants.http_status import SUCCESS, NOT_FOUND, SERVER_ERROR
 from ecommerce.utils.response_helper import create_response
 from ecommerce.services.order_service import list_orders, create_order, update_order, delete_order
 
-@frappe.whitelist(allow_guest=False)
+@frappe.whitelist(allow_guest=True)
 def get_orders(user_id):
    return list_orders(user_id)
 
 
-@frappe.whitelist(allow_guest=False)
+@frappe.whitelist(allow_guest=True)
 def create_new_order(shipping_address, lga, post_code, subtotal, shipping_fee, discount, total, payment_method, user_id, status):
     return create_order(shipping_address, lga, post_code, subtotal, shipping_fee, discount, total, payment_method, user_id, status)
 
 
-@frappe.whitelist(allow_guest=False)
+@frappe.whitelist(allow_guest=True)
 def modify_order(order_id, status,  item_name, quantity):
     return update_order(order_id, status,  item_name, quantity)
 
 
-@frappe.whitelist(allow_guest=False)
+@frappe.whitelist(allow_guest=True)
 def remove_order(order_id):
     return delete_order(order_id)
