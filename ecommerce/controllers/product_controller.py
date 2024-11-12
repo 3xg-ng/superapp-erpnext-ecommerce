f568c5872626e03af88fae1ff40f845bcc82a6b9
Taiwo Olapade <44630802+Semiteprofessor@users.noreply.github.com>
2024-11-12 08:36:52 +0100

    Commit

--- ecommerce/controllers/product_controller.py
+++ ecommerce/controllers/product_controller.py
@@ -23,46 +23,6 @@ def get_all_items_by_category_limit():
     return list_items_category()
 
 
-# Smartphones
-@frappe.whitelist(allow_guest=True)
-def get_all_items_by_smartphone():
-    return list_items_smartphone()
-
-# Accessories
-@frappe.whitelist(allow_guest=True)
-def get_all_items_by_accessories():
-    return list_items_accessories()
-
-# Laptops
-@frappe.whitelist(allow_guest=True)
-def get_all_items_by_laptops():
-    return list_items_laptops()
-
-# Home appliances
-@frappe.whitelist(allow_guest=True)
-def get_all_items_by_home_appliance():
-    return list_items_home_appliance()
-
-# Kiddies
-@frappe.whitelist(allow_guest=True)
-def get_all_items_by_kiddies():
-    return list_items_kiddies()
-
-# New Arrival
-@frappe.whitelist(allow_guest=True)
-def get_all_items_by_new_arrival():
-    return list_items_new_arrival()
-
-# Best Seller
-@frappe.whitelist(allow_guest=True)
-def get_all_items_by_best_seller():
-    return list_items_best_seller()
-
-# Official Store
-@frappe.whitelist(allow_guest=True)
-def get_all_items_by_official_store():
-    return list_items_official_store()
-
 @frappe.whitelist(allow_guest=True)
 def get_single_item(item_code):
     return get_item_by_code(item_code)
