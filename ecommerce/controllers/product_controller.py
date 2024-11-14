2b658e80bb3e49c5a619a26f3bd3296350751914
Taiwo Olapade <44630802+Semiteprofessor@users.noreply.github.com>
2024-11-14 06:46:32 +0100

    Commit

--- ecommerce/controllers/product_controller.py
+++ ecommerce/controllers/product_controller.py
@@ -1,6 +1,6 @@
 import frappe
 
-from ecommerce.services.product_service import list_items, list_items_category, get_item_by_code, add_new_item, update_item_by_code, delete_item_by_code, fetch_similar_items, add_to_wishlist, remove_from_wishlist, get_wishlist
+from ecommerce.services.product_service import list_items, list_items_category, get_item_by_code, add_new_item, update_item_by_code, delete_item_by_code, add_to_wishlist, remove_from_wishlist, get_wishlist
     
 @frappe.whitelist(allow_guest=True)
 def get_all_items(product_name=None, min_price=None, max_price=None, color=None, category=None, brand=None, rating=None, discount=None):
@@ -16,10 +16,6 @@ def get_all_items(product_name=None, min_price=None, max_price=None, color=None,
     }
     return list_items(filters)
 
-
-@frappe.whitelist(allow_guest=True)
-def get_product_details(item_code=None):
-    return fetch_similar_items(item_code)
 
 @frappe.whitelist(allow_guest=True)
 def get_all_items_by_category_limit():
