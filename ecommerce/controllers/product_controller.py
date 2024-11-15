d97a38d0eb887c55adc8ed958a5ae7467719b27b
Taiwo Olapade <44630802+Semiteprofessor@users.noreply.github.com>
2024-11-15 12:51:45 +0100

    Commit

--- ecommerce/controllers/product_controller.py
+++ ecommerce/controllers/product_controller.py
@@ -1,6 +1,6 @@
 import frappe
 
-from ecommerce.services.product_service import list_items, list_items_category, get_item_by_code, add_new_item, update_item_by_code, delete_item_by_code, add_to_wishlist, remove_from_wishlist, get_wishlist
+from ecommerce.services.product_service import list_items, list_items_category, get_item_by_code, add_new_item, update_item_by_code, delete_item_by_code, list_single_item
     
 @frappe.whitelist(allow_guest=True)
 def get_all_items(product_name=None, min_price=None, max_price=None, color=None, category=None, brand=None, rating=None, discount=None):
@@ -25,6 +25,11 @@ def get_all_items_by_category_limit():
 @frappe.whitelist(allow_guest=True)
 def get_single_item(item_code):
     return get_item_by_code(item_code)
+
+
+@frappe.whitelist(allow_guest=True)
+def get_single_item():
+    return list_single_item()
 
 @frappe.whitelist(allow_guest=True)
 def add_item(item_code, product_name, category, old_price, new_price, image, rating, brand, description, dimension, display_type, resolution, features, chipset, cpu, internal_memory, ram, battery_type, battery_life, charging, magsafe_charging, collection, model, weight, availability, color, quantity, warranty):
