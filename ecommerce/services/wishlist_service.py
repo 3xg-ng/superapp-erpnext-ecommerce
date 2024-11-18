6c0fd7360782d84f9b924533e81e015528845697
Taiwo Olapade <44630802+Semiteprofessor@users.noreply.github.com>
2024-11-18 23:37:35 +0100

    Commit

--- ecommerce/services/wishlist_service.py
+++ ecommerce/services/wishlist_service.py
@@ -2,34 +2,40 @@ import frappe
 from ecommerce.constants.http_status import SUCCESS, NOT_FOUND, SERVER_ERROR
 from ecommerce.utils.response_helper import create_response
 
-### Get all items
+### Add to wishlist
 def add_to_wishlist(user_id, item_code):
     try:
-        # Check if item is already in the wishlist
-        existing_entry = frappe.db.sql("""
-            SELECT name FROM `tabProductWishlist`
-            WHERE user_id = %s AND item_code = %s
-        """, (user_id, item_code), as_dict=True)
+        product_details = frappe.db.get_value(
+            "Products", 
+            {"item_code": item_code}, 
+            ["product_name", "new_price", "image"], 
+            as_dict=True
+        )
 
-        if existing_entry:
-            return create_response(SUCCESS, "Item already in wishlist.")
+        if not product_details:
+            return create_response(NOT_FOUND, "Product not found.")
 
-        # Insert new item into the wishlist
-        wishlist_entry = frappe.get_doc({
-            "doctype": "Wishlist",
-            "user_id": user_id,
-            "item_code": item_code,
-            "date_added": frappe.utils.now()
-        })
+        wishlist_item = frappe.db.get_value("ProductWishlist", {"user_id": user_id, "item_code": item_code})
 
-        wishlist_entry.insert()
+        
+        new_wishlist_item = frappe.get_doc({
+                "doctype": "ProductWishlist",
+                "user_id": user_id,
+                "item_code": item_code,
+                "product_name": product_details["product_name"],
+                "price": product_details["new_price"],
+                "image": product_details["image"],
+            })
+        new_wishlist_item.insert()
+
         frappe.db.commit()
+        return create_response(SUCCESS, f"Item {item_code} added to wishlist successfully!")
 
-        return create_response(SUCCESS, "Item added to wishlist successfully.")
-
+    except frappe.DuplicateEntryError:
+        return create_response(SERVER_ERROR, "Duplicate entry found. Please try again.")
     except Exception as e:
-        frappe.log_error(message=str(e), title="Error adding to wishlist")
-        return create_response(SERVER_ERROR, f"An unexpected error occurred: {str(e)}")
+        frappe.log_error(f"Error adding item {item_code} to wishlist for user {user_id}: {str(e)}", "Add to wishlist Error")
+        return create_response(SERVER_ERROR, f"An unexpected error occurred while adding the item: {str(e)}")
 
 
 def remove_from_wishlist(user_id, item_code):
