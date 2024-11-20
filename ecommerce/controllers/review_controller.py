d79400b0c9ed212ce9300845a9cb790eca0df621
Taiwo Olapade <44630802+Semiteprofessor@users.noreply.github.com>
2024-11-20 19:21:43 +0100

    Commit

--- ecommerce/controllers/review_controller.py
+++ ecommerce/controllers/review_controller.py
@@ -3,8 +3,8 @@ import frappe
 from ecommerce.services.review_service import create_product_review, list_reviews
 
 @frappe.whitelist(allow_guest=True)
-def get_review(user_id):
-    return list_reviews(user_id)
+def get_review(item_code):
+    return list_reviews(item_code)
 
 
 @frappe.whitelist(allow_guest=True)
